0a3d41b1efb021018904d5010b3e90cf284ae0fe
SSzretter <sszretter@hotmail.com>
2016-11-13 17:33:02 -0500

    IU-2016.2 <Scott@cmatefire8 Overwrite remote https://github.com/SSzretter/scrps.git to local

--- utils.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    isPosEqual : function(firstPos,secondPos){
-        if(!(firstPos && secondPos)){
-            return false
-        }
-        return firstPos.x === secondPos.x && firstPos.y === secondPos.y && firstPos.roomName === secondPos.roomName
-    }
-}
